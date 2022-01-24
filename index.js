ac6529fc78a8f4e68d99a9e31f02669e578a36f5
Tiago Ferreira <52896114+tiagoferreira18@users.noreply.github.com>
2022-01-24 10:28:59 -0300

    Configuração de rotas api

--- index.js
+++ index.js
@@ -8,8 +8,40 @@ app.get('/', (req, res) => {
     res.send(`API de alunos com Node.js, Express e MySQL`);
 });
 
+//Endpoint (rota) para todos os alunos da API
+app.get('/alunos', (req,res)=>{
+    res.send(`Dados de todos os alunos`);
+});
+
+// Endpoint (rotas) para dados de Um aluno da API
+app.get('/alunos/:id', (req, res)=>{
+    res.send(`Dados de UM aluno`)
+});
+
+
+//POST: Endpoint parar inserir novos alunos
+app.post('/alunos', (req,res)=>{
+    res.send(`Inserindo um aluno`);
+});
+//PUT: Endpoint para atualizar todos os dados de um aluno
+// app.put('/alunos/:id', (req, res)=>{
+//     res.send(`Atualizando todos os Dados de UM aluno`)
+// });
+
+//PATCH: Endpoint para atualizar todos/alguns dados de um aluno
+app.patch('/alunos/:id', (req, res)=>{
+    res.send(`Atualizando alguns ou todos Dados de UM aluno`)
+});
+//DELETE: Endpoint para excluir alunos
+app.delete('/alunos/:id', (req, res)=>{
+    res.send(`Excluindo UM alunos`)
+});
+
+
+
 
 // Configurando a execução do servidor Express
 app.listen(porta, ()=>{
     console.log(`Servidor rodando na porta ${porta}`);
 });
+

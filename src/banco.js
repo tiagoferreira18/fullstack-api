6caa892bed272391c1a99fb91c37ca834cffd3c3
Tiago Ferreira <52896114+tiagoferreira18@users.noreply.github.com>
2022-01-26 10:31:21 -0300

    update dos dados da api

--- src/banco.js
+++ src/banco.js
@@ -1,10 +1,17 @@
 import mysql from 'mysql2';
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'escola'
+    /* local */
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'escola'
+
+    /* remoto */
+    host: 'db4free.net',
+    user: 'admin_api_tiagof',
+    password: 'senac*123',
+    database: 'escola_tiago_fer'
 
 });
 
@@ -12,7 +19,7 @@ conexao.connect(erro =>{
     if(erro){
         console.log(`Erro ao conectar: ${erro.message}`);
     }else{
-        console.log(`Banco de dados conectado com sucesso!`);
+        console.log(`Banco de dados conectado em: ${conexao.config.host}`);
     }
 });
 
